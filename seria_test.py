c5cc88eccedbbaba611d9a18ee75a1f0d3cca639
Enmar123 <js_enmar1@hotmail.com>
2018-04-21 23:00:07 +0000

    6th Commit - Cleanup
    Performed Cleanup on some files and remove unescessary test documents.

--- seria_test.py
+++ seria_test.py
@@ -4,45 +4,39 @@ import numpy
 from time import sleep
 
 ser = serial.Serial('/dev/ttyACM0',9600) 
-s = [0,1]
-sleep(.1)
-q = (0,1)
+sleep(1)
 
 # PI -> INO chars (works) ----------------------------------------------
-#while True:
-	#var1 = 'A'
-	#var2 = 'B'
+
+while True:
+	var1 = 'A'
+	var2 = 'B'
 	
-	#ser.write(bytes(var1, 'UTF-8'))
-	#sleep(1)
-	#ser.write(bytes(var2, 'UTF-8'))
-	#sleep(1)
+	ser.write(bytes(var1, 'UTF-8'))
+	sleep(1)
+	ser.write(bytes(var2, 'UTF-8'))
+	sleep(1)
 
 # INO -> PI char (works)------------------------------------------------
 
-datastore = [1,1]
-while True:
-	datain = ser.readline()
-	datain = chr(int(datain, 16))
-	print(datain)
+#datastore = [1,1]
+#while True:
+	#datain = ser.readline()
+	#datain = chr(int(datain, 16))
+	#print(datain)
 
-	if(datain == 'B'):
-		print('IT REGISTERS MWAHAHAH!')
-
-
-
-while True:
-	datain = str(int (ser.readline(),16))
-	print(datain)
+	#if(datain == 'B'):
+		#print('IT REGISTERS MWAHAHAH!')
 
 ## INO -> PI nums (works) -----------------------------------------------
-#while True:
-	#datain = str(int (ser.readline(),16)
-	#datain = int(datain)
-	#print(datain)
+
+while True:
+	data = str(int (ser.readline(),10))
+	data = int(data)
+	print(data)
 	
-	#mult = datain/10
-	#if (mult).is_integer():
-		#print('Hey this number is a multiple of 10!')
+	mult = data/10
+	if (mult).is_integer():
+		print('Hey this number is a multiple of 10!')
 	
 	
--- serial_test/serial_test.ino
+++ serial_test/serial_test.ino
@@ -1,46 +1,52 @@
+// Variable definitions for the codes below (space separated)
+
+char datain = ' ';
+
+char datastring[50] = {0};
+char dataout;
 
 int a = 0;
-int bump = 0;
-char datain = ' ';
-char datastring[50] = {0};
+char data[1];
 
-//char datain[1];
+//-----------------------------------------------------------------
 
 void setup() {
 Serial.begin(9600);              //Starting serial communication
 }
-  
+
+//------------------------------------------------------------------  
+
 void loop() {
 
 //// PI->INO chars (works) -----------------------------------------
-//if(Serial.available()){
-//  datain = char(Serial.read());
-//  Serial.println();
-//  Serial.println(datain);
-//  delay(250);
-//}
+
+if(Serial.available()){
+  datain = char(Serial.read());
+  Serial.println();
+  Serial.println(datain);
+  delay(250);
+}
   
- 
-// INO -> PI char (works) ------------------------------------------
-char datastring[50] = {0};
-char dataout;
-
-dataout = 'A';
-sprintf(datastring,"%02X",dataout);
-Serial.println(datastring);
-delay(1000);             // "Spent" terminal windows cause slowdowns
-
-dataout = 'B';
-sprintf(datastring,"%02X",dataout);
-Serial.println(datastring);
-delay(1000);
-
-
-
-//// INO -> PI nums (works) --------------------------------------
-//a = a + 1;                    // max count is up to +- 32767
-//String dataout = String(a);
-//Serial.println(data);
+//// INO -> PI char (works) ------------------------------------------
+//
+//char datastring[50] = {0};
+//char dataout;
+//
+//dataout = 'A';
+//sprintf(datastring,"%02X",dataout);
+//Serial.println(datastring);
 //delay(1000);             // "Spent" terminal windows cause slowdowns
+//
+//dataout = 'B';
+//sprintf(datastring,"%02X",dataout);
+//Serial.println(datastring);
+//delay(1000);
+
+// INO -> PI nums (works) --------------------------------------
+
+//a = a + 1;                    // max count is up to +- 32767
+//String data = String(a);
+//Serial.println(data);
+//delay(100);             // "Spent" terminal windows -> slowdowns
 
 }
