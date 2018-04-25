975aec52fa2e7ca69b6988d578af2003339f7ffd
Enmar123 <js_enmar1@hotmail.com>
2018-04-24 22:35:46 -0400

    1st Commit
    Testing out orphan branches

---
+++ inocode/inocode.ino
@@ -0,0 +1,194 @@
+//Line Tracking IO define ---------------------------------
+#include <Servo.h>  //servo library
+Servo myservo;      // create servo object to control servo  
+
+int Echo = A4;  
+int Trig = A5; 
+
+#define LT_R !digitalRead(10)
+#define LT_M !digitalRead(4)
+#define LT_L !digitalRead(2)
+
+#define ENA 5
+#define ENB 6
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
+
+// USER INPUTS --------------------------------------------
+
+#define carSpeed 150
+#define delay1 100
+#define turnSpeed 250
+#define turntime 360
+#define forwardtime 2150
+int rightDistance = 0, leftDistance = 0, middleDistance = 0;
+
+// Function Setup -----------------------------------------
+void avoidR(){ 
+  right();
+  delay(turntime+40);
+  forward();              //go forward
+  delay(forwardtime);          //delay 1000 ms
+  left();                 //turning left
+  delay(turntime);
+  forward();              //go forward
+  delay(forwardtime);          //delay 1000 ms
+  left();                 //turning left
+  delay(turntime);
+  forward();              //go forward
+  delay(forwardtime-750);      //delay 1000 ms
+  //right();                //turning left
+  //delay(turntime);
+  //forward();              //go forward
+  //delay(forwardtime-750);      //delay 1000 ms
+}
+
+void avoidL(){
+  left();
+  delay(turntime+40);
+  forward();              //go forward
+  delay(forwardtime);          //delay 1000 ms
+  right();                //turning left
+  delay(turntime);
+  forward();              //go forward
+  delay(forwardtime);          //delay 1000 ms
+  right();                //turning left
+  delay(turntime);
+  forward();              //go forward
+  delay(forwardtime-750);      //delay 1000 ms
+  //left();                 //turning left
+  //delay(turntime);
+  //forward();              //go forward
+  //delay(forwardtime-750);      //delay 1000 ms
+}
+
+void forward(){ 
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  analogWrite(IN1, 240);
+  analogWrite(IN2, 0);
+  analogWrite(IN3, 0);
+  analogWrite(IN4, 255);
+  Serial.println("Forward");
+}
+
+void back() {
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  analogWrite(IN1, 0);
+  analogWrite(IN2, 255);
+  analogWrite(IN3, 255);
+  analogWrite(IN4, 0);
+  Serial.println("Back");
+}
+
+void left() {
+  analogWrite(ENA, turnSpeed);
+  analogWrite(ENB, turnSpeed);
+  analogWrite(IN1, 0);
+  analogWrite(IN2, 255);
+  analogWrite(IN3, 0);
+  analogWrite(IN4, 255); 
+  Serial.println("Left");
+}
+
+void right() {
+  analogWrite(ENA, turnSpeed);
+  analogWrite(ENB, turnSpeed);
+  analogWrite(IN1, 255);
+  analogWrite(IN2, 0);
+  analogWrite(IN3, 255);
+  analogWrite(IN4, 0);
+  Serial.println("Right");
+}
+
+void stop() {
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+  Serial.println("Stop!");
+} 
+
+//Ultrasonic distance measurement Sub function
+int Distance_test() {
+  digitalWrite(Trig, LOW);   
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);  
+  delayMicroseconds(20);
+  digitalWrite(Trig, LOW);   
+  float Fdistance = pulseIn(Echo, HIGH);  
+  Fdistance= Fdistance / 58;       
+  return (int)Fdistance;
+}  
+
+//----------------------------------------------------------------
+
+void setup(){
+  myservo.attach(3);  // attach servo on pin 3 to servo object
+  Serial.begin(9600);
+  pinMode(LT_R,INPUT);
+  pinMode(LT_M,INPUT);
+  pinMode(LT_L,INPUT);
+  pinMode(Echo, INPUT);    
+  pinMode(Trig, OUTPUT);  
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  stop();
+} 
+
+// Main Control Loop ------------------------------------------------
+
+// Note: Black line = 1 or high
+void loop() { 
+   //myservo.write(90);  //(initial 90/modified to 125)setservo position according to scaled value
+    //delay(500); 
+    middleDistance = Distance_test();
+
+if(middleDistance <= 27) {     
+      stop();
+      delay(500);                         
+      myservo.write(10);          
+      delay(1000);      
+      rightDistance = Distance_test();
+      
+      delay(500);
+      myservo.write(90);     //initial 90/modified to 125         
+      delay(1000);                                                  
+      myservo.write(180);              
+      delay(1000); 
+      leftDistance = Distance_test();
+      
+      delay(500);
+      myservo.write(90);     //initial 90/modified to 125         
+      delay(1000);
+      
+      if(rightDistance > leftDistance) {
+        avoidR();
+      }
+      else if(rightDistance < leftDistance) {
+        avoidL();
+      }
+      else if((rightDistance <= 20) || (leftDistance <= 20)) {
+        back();
+        delay(180);
+      }
+  }
+      
+  else if(LT_M ){
+    forward();
+  }
+  else if(LT_R || (LT_R && LT_M)) { 
+    right();
+    while(LT_R);                             
+  }   
+  else if(LT_L || (LT_L && LT_M)) {
+    left();
+    while(LT_L);  
+  }
+
+}  
---
+++ visioncode.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+# EML ???? Smart Machine Design and Development
+# Florida International University
+# Department of Mechanical and Materials Engineering
+# Jason Soto <jsoto103@fiu.edu>
+# Miami, Spring 2018
+
+# USER INPUTS ----------------------------------------------------------
+frame_w_pix = 640
+frame_h_pix = 480
+#pix_width  = 640
+#pix_height = 480
+
+safezone_pix = 100
+
+hudcolor = (255,255,255)
+
+# PROGRAM SETUP --------------------------------------------------------
+ 
+import cv2 as cv
+import numpy as np
+import time
+
+# RASPI AND ARDUCAM
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import serial
+
+ser = serial.Serial('/dev/ttyACM0',9600)
+
+
+
+# FUNCTIONS ------------------------------------------------------------
+
+# Define function that computes centroid of monochrome blob (mask)
+def centroid( mask ):
+	M = cv.moments(mask,True)
+	m00 = M['m00']
+	if m00 == 0: # Ops, don't want divide by zero!
+		x = -100 
+		y = -100
+	else:
+		x = int( M['m10']/m00 )
+		y = int( M['m01']/m00 )
+	return x,y
+
+def weightfcn( x ):
+	offset = 1*x - 640/2
+	weight = int( offset * 255/(640/2) )
+	return weight
+	
+
+# PRIMARY LOOP --------------------------------------------------------- 
+
+camera = PiCamera()
+camera.vflip = True
+camera.resolution = (frame_w_pix, frame_h_pix)
+camera.framerate = 24
+rawCapture = PiRGBArray(camera, size=(frame_w_pix, frame_h_pix))
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+
+
+	
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	t0 = time.time()
+	image = frame.array
+	
+	# Exit if any key is pressed
+	key = cv.waitKey(1) & 0xFF
+	if key == ord("q"):
+		cv.destroyAllWindows()
+		break
+	
+	rawCapture.truncate(0)
+
+	
+	# Image Processing
+	hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)	# image from BGR to HSV color
+	
+	#color = 46 						# lime green color
+	#color = 91 * (176/255)				# Brown/green Chip bag color
+	#color = 64 * (176/255) 				# Yellow bag color
+	#color = 155 * (176/360)
+	#color = 97 * (176/360)
+	#color = 200 * (176/360) 			# Blue plastic bag
+	color1 = 0 * (179/360)				# Red Coca-Cola Can
+	color2 = 360 * (179/360)			# Red Coca-Cola Can
+	
+	margin = 5
+	
+	bound1_low = (color1 - margin, 80, 80)
+	bound1_up = (color1 + margin, 255, 255)
+
+	bound2_low = (color2 - margin, 80, 80)
+	bound2_up = (color2 + margin, 255, 255)
+	
+	#colorLower = (color - 10, 80, 80)
+	#colorUpper = (color + 10, 255, 255)
+	
+	mask1 = cv.inRange(hsv, bound1_low,bound1_up )
+	mask2 = cv.inRange(hsv, bound2_low, bound2_up)
+	mask = mask1 + mask2
+	
+	#mask = cv.inRange(hsv, colorLower, colorUpper)
+	mask = cv.erode(mask, None, iterations = 2)
+	mask = cv.dilate(mask, None, iterations = 2)
+	
+	hud = image
+	
+	# Data extraction
+	x,y = centroid(mask)
+
+	n_white_pix = np.sum(mask == 255)
+	
+	rows = len(mask)
+	columns = len(mask[0])
+	
+	weight = weightfcn(x)
+	
+	# Building HUD
+	
+	xmid = int(frame_w_pix/2)
+	ymid = int(frame_h_pix/2)
+	offset = int(safezone_pix/2)
+	
+	cv.line(hud, (xmid-offset, 1), (xmid-offset, frame_h_pix), hudcolor, 5)
+	cv.line(hud, (xmid+offset, 1), (xmid+offset, frame_w_pix), hudcolor, 5)
+	
+	cv.circle(hud, (xmid,ymid), 6, (0,0,255), 3)
+	
+	cv.circle(hud, (x,y), 10, (0,0,255), 10)
+	
+	# PI->INO Char Data out
+	
+	if(x == -100):
+		dataout = 'O'
+	elif(x >= (xmid+offset)):
+		dataout = 'L'
+	elif(x <= (xmid-offset)):
+		dataout = 'R'
+	else:
+		dataout = 'M'
+	
+	ser.write(bytes(dataout, 'UTF-8'))
+	
+	# Feedback
+	#cv.imshow('Default Video Feed', image)
+	cv.imshow('Color Mask', mask)
+	cv.imshow('Display HUD', hud)
+	t1 = time.time()
+	total = t1-t0
+	
+	print('--------------------------------------')
+	print('Number of white pixels:', n_white_pix)
+	print('Centroid Location (X,Y)', [x,y])
+	print('PWM Speed Weight       ', weight)
+	print('Char Data out          ', dataout)
+	print('Cycle time             ', total)
+	
+	#print('Size of image box      ', [rows, columns])
+	#print('Arduino Data'		   , ino_data)				# not working yet
+	
+	#print('Data array', [n_white_pix, x, y])
+	
+# Clean resources
+rawCapture.truncate(0)
+cap.release()
+cv.destroyAllWindows()
+print("Program Terminated.")
